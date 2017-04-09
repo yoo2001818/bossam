1fdbf794d011d81d934d26ab7e3122c98441f03c
Duknam Yoo <yoo2001818@gmail.com>
2017-04-09 22:52:53 +0900

    Add index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-// TODO
+import tokenize from './tokenize';
+import parse from './parse';
+import compile from './compile';
+
+export default function compileFromCode(code, namespace) {
+  return compile(parse(tokenize(code)), namespace);
+}
